f6ebb2ea2ffb75502858fc7042d12cf798c8fb1f
Ivan Cruz <ivancr8824@gmail.com>
2024-03-12 12:58:38 -0500

    implementing appFlow

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,14 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'myApp',
   webDir: 'www',
+  plugins: {
+    LiveUpdates: {
+      appId: 'bdba1109',
+      channel: 'testing-destination',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
+  },
   server: {
     androidScheme: 'https'
   }
